b05c0d43c601fbc3ef2b89c1f7a7c5b99eed8e6b
Gerardo Villegas <villegasgerardo.j@gmail.com>
2020-06-27 21:20:06 -0500

    Primer Commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,17 @@
+export const getGifs = async (category) => {
+	const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(
+		category
+	)}&limit=10&api_key=eY1r6cgPP5xej3BwSTl6GoF1E6qdb1ON`;
+	const resp = await fetch(url);
+
+	const { data } = await resp.json();
+
+	const gifs = data.map((img) => {
+		return {
+			id: img.id,
+			title: img.title,
+			url: img.images?.downsized_medium.url,
+		};
+	});
+	return gifs;
+};
